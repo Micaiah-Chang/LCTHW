01f5bb8012646ecc2f44ec4cf9f70a17b497a360
Micaiah-Chang <Micaiah@server.fake>
2013-05-21 01:18:48 -0700

    Learn C the Hard Way Gaiden

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("Hi Sytha.");
+  puts("Morning");
+  puts("I am so tsundere.");
+  puts("Rawr.");
+  puts("Sexy time");
+  return 0;
+}
