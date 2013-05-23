2fe8ed9338228c200a61940fe1707338a631084d
Micaiah-Chang <Micaiah@server.fake>
2013-05-22 21:09:51 -0700

    Added a .gitignore

--- ex4.c
+++ ex4.c
@@ -5,9 +5,9 @@
 int main()
 {
   int age = 10;
-  int height;
+  int height = 71;
 
-  printf("I am %d years old.\n");
+  printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
 
   return 0;
