9f51d5efaafe230d1d163c81d9f5674b8140a0fc
Micaiah-Chang <Micaiah@server.fake>
2013-05-26 20:59:27 -0700

    Exercise 7 done

--- Makefile
+++ Makefile
@@ -1,24 +1,29 @@
 CFLAGS=-Wall -g
 
-all:
-	cc ex1.c -o ex1
-	cc ex2.c -o ex2
-	cc ex4.c -o ex4
-
 clean:
-	rm -f ex1 ex2 ex4 ex5
+	rm -f ex1 ex2 ex4 ex5 ex6 ex7
 
-ex1:
-	cc ex1.c -o ex1
+ex7:
+	cc ex7.c -o ex7
 
-ex2:
-	cc ex2.c -o ex2
-
-ex4:
-	cc ex4.c -o ex4
+ex6:
+	cc ex6.c -o ex6
 
 ex5:
 	cc ex5.c -o ex5
 
-ex6:
+ex4:
+	cc ex4.c -o ex4
+
+ex2:
+	cc ex2.c -o ex2
+
+ex1:
+	cc ex1.c -o ex1
+
+all:
+	cc ex1.c -o ex1
+	cc ex2.c -o ex2
+	cc ex4.c -o ex4
 	cc ex6.c -o ex6
+	cc ex7.c -o ex7
---
+++ ex7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int bugs = 100;
+  double bug_rate = 1.2;
+
+  printf("You have %d bugs at the imaginary rate of %f.\n",
+		 bugs, bug_rate);
+
+  long universe_of_defects = 100000000000L;
+  printf("The entire universe has %ld bugs.\n",
+		 universe_of_defects);
+
+  double expected_bugs = bugs * bug_rate;
+  printf("You are expected to have %f bugs.\n",
+		 expected_bugs);
+
+  double part_of_universe = expected_bugs / universe_of_defects;
+  printf("That is only a %E portion of the universe.\n", part_of_universe);
+
+  // this makes no sense, just a demo of something weird
+  char nul_byte = '\0';
+  int care_percentage = bugs * nul_byte;
+  printf("Which means you should care %d%%.\n",
+		 care_percentage);
+  printf("Nul_byte char: %c\n", nul_byte);
+  printf("Nul_byte string: %s\n", nul_byte);
+
+
+  
+  return 0;
+}
