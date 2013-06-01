a9c289b217ac88c112642464dce57fb5d9bace99
Micaiah-Chang <Micaiah@server.fake>
2013-05-31 23:36:38 -0700

    Exercise 10

--- ex8.c
+++ ex8.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-  int areas[] = {10, 12, 13, 14, 20};
-  char name[] = "Zed";
   char full_name[] = {
 	'Z','e', 'd',
 	' ', 'A', '.', ' ',
-	'S', 'h', 'a', 'w', '\0'
+	'S', 'h', 'a', 'w', '\0' 
   };
-
+  int areas[] = {10, 12, 13, 14, 20};
+  char name[] = "Zed";
+	
   // WARNING: On some systems you may have to change the
   // %ld in this code to a %u since it will use unsigned ints
   printf("The size of an int: %u\n", sizeof(int));
