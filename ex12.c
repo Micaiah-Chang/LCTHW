387b211d0d5b659edb2a9b9b413ba90c87d8ab6a
Micaiah-Chang <Micaiah@server.fake>
2013-06-07 13:35:35 -0700

    Up to exercise 13 & figuring out ASCII <-> Integer mappings

--- ex12.c
+++ ex12.c
@@ -9,14 +9,13 @@ int main(int argc, char *argv[])
 	 } else if(argc > 1 && argc < 4) {
 		  printf("Here's your arguments:\n");
 
-		  for(i = 0; i < argc; i++) {
+		  for(i = 1; i < argc; i++) {
 			   printf("%s ", argv[i]);
 		  }
 		  printf("\n");
 	 } else {
-		  printf("You have too many arguments. You suck\n");
+	 	  printf("You have too many arguments. You suck\n");
 	 }
 
 	 return 0;
 }
-	 
