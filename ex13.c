443c859945135ef47bf24240fccffab2eede0a1f
Micaiah Chang <ahchang012@gmail.com>
2013-06-07 16:21:52 -0700

    Finished 13

--- ex13.c
+++ ex13.c
@@ -9,52 +9,67 @@ int main(int argc, char *argv[])
 	 }
 
 	 int i = 0;
-	 for(i = 0; argv[1][i] != '\0'; i++){
-		  char letter = argv[1][i];
+	 char letter;
+	 for(i = 0, letter = argv[1][i];
+		 argv[1][i] != '\0';
+		 i++, letter = argv[1][i]){
+		  // char letter = argv[1][i];
+		  if((int)letter < 91) {
+			   letter += 32;
+		  }
 
-		  letter -= 1;
-		  printf("%c", letter);
+	 	  switch(letter) {
+	 	  case 'a':
+	 		   printf("%d: 'A'\n", i);
+	 		   break;
+
+	 	  case 'e':
+	 		   printf("%d: 'E'\n", i);
+	 		   break;
+			   
+	 	  case 'i':
+	 		   printf("%d: 'I'\n", i);
+	 		   break;
+
+	 	  case 'o':
+	 		   printf("%d: 'O'\n", i);
+	 		   break;
+
+	 	  case 'u':
+	 		   printf("%d: 'U'\n", i);
+	 		   break;
+
+	 	  case 'y':
+	 		   if(i > 2){
+	 				// it's only sometimes Y
+	 				printf("%d: 'Y'\n", i);
+	 				break;
+	 		   }
+			   
+
+	 	  default:
+	 		   printf("%d: %c is not a vowel\n", i, letter);
+	 	  }
 
 		  
-		  switch(letter) {
-		  case 'a':
-		  case 'A':
-			   printf("%d: 'A'\n", i);
-			   break;
+	 /* if(letter == 'a'){ */
+	 /* 	  printf("%d: 'A'\n", i); */
+	 /* } else if(letter == 'e'){ */
+	 /* 	  printf("%d: 'E'\n", i); */
+	 /* } else if(letter == 'i'){ */
+	 /* 	  printf("%d: 'I'\n", i); */
+	 /* } else if(letter == 'o'){ */
+	 /* 	  printf("%d: 'O'\n", i); */
+	 /* } else if(letter == 'u'){ */
+	 /* 	  printf("%d: 'U'\n", i); */
+	 /* } else if(letter == 'y' && i > 2){ */
+	 /* 	  printf("%d: 'Y'\n", i); */
+	 /* } else { */
+	 /* 	  printf("%d: %c is not a vowel\n", i, letter); */
+	 /* } */
 
-		  case 'e':
-		  case 'E':
-			   printf("%d: 'E'\n", i);
-			   break;
-			   
-		  case 'i':
-		  case 'I':
-			   printf("%d: 'I'\n", i);
-			   break;
 
-		  case 'o':
-		  case 'O':
-			   printf("%d: 'O'\n", i);
-			   break;
-
-		  case 'u':
-		  case 'U':
-			   printf("%d: 'U'\n", i);
-			   break;
-
-		  case 'y':
-		  case 'Y':
-			   if(i > 2){
-					// it's only sometimes Y
-					printf("%d: 'Y'\n", i);
-					break;
-			   }
-			   
-
-		  default:
-			   printf("%d: %c is not a vowel\n", i, letter);
-		  }
+		  
 	 }
-
 	 return 0; 
 }
