4a2c99f1320a95f1ed245551f395226ccad8b454
Micaiah-Chang <Micaiah@server.fake>
2013-06-07 19:18:12 -0700

    Exercise 14 done

---
+++ ex14.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+//forward declarations
+void print_letters(char arg[], int length);
+
+void print_arguments(int argc, char* argv[])
+{
+	 int i = 0;
+	 
+	 for(i = 0; i < argc; i++) {
+		  print_letters(argv[i], strlen(argv[i]));
+	 }
+}
+
+void print_letters(char arg[], int length)
+{
+	 int i = 0;
+
+	 for(i = 0; i < length; i++) {
+		  char ch = arg[i];
+
+		  if(isalpha(ch) || isblank(ch)) {
+			   printf("'%c' == %d ", ch, ch);
+		  }
+	 }
+
+	 printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	 print_arguments(argc, argv);
+	 return 0;
+}
