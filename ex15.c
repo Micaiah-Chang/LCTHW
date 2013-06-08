e59ea8010d5287d43b5620e3205499649bb7d5c5
Micaiah-Chang <Micaiah@server.fake>
2013-06-07 19:18:12 -0700

    Exercise 14 done

---
+++ ex15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	 // create two arrays we care about
+	 int ages[] = {23, 43, 12, 89, 2};
+	 char *names[] = {
+		  "Alan", "Frank",
+		  "Mary", "John", "Lisa"
+	 };
+
+	 // safely
+}
