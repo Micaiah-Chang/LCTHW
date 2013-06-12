62231cd6bde6a66f0817d416ee529b93080b12f1
Micaiah-Chang <Micaiah@server.fake>
2013-06-12 02:18:59 -0700

    Skipping ahead to 16, will do 15 EC later

--- ex15.c
+++ ex15.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	 int *cur_age = ages;
 	 char **cur_name = names;
 
+
 	 // second way using pointers
 	 for(i = 0; i < count; i++) {
 		  printf("%s is %d years old.\n",
@@ -49,6 +50,7 @@ int main(int argc, char *argv[])
 		  printf("%s lived %d years so far.\n",
 				 *cur_name, *cur_age);
 	 }
+	 
 
 	 return 0;
 }
