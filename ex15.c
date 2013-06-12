752c33c2b97b592d1b9183a21323114ccb657d2d
Micaiah Chang <ahchang012@gmail.com>
2013-06-12 11:24:43 -0700

    How to break finished for 15

--- ex15.c
+++ ex15.c
@@ -12,9 +12,10 @@ int main(int argc, char *argv[])
 	 // safely get the size of ages
 	 int count = sizeof(ages) / sizeof(int);
 	 int i = 0;
-
+	 
 	 // first way of using indexing
-	 for(i = 0; i < count; i++) {
+	 for(i = count - 1; i >= 0; i--) {
+		  // printf("Count: %d\n", count);
 		  printf("%s has %d years alive.\n",
 				 names[i], ages[i]);
 	 }
@@ -27,7 +28,7 @@ int main(int argc, char *argv[])
 
 
 	 // second way using pointers
-	 for(i = 0; i < count; i++) {
+	 for(i = count - 1; i >= 0; i--) {
 		  printf("%s is %d years old.\n",
 				 *(cur_name+i),*(cur_age+i));
 	 }
@@ -35,7 +36,7 @@ int main(int argc, char *argv[])
 	 printf("---\n");
 
 	 // third way, pointers are just arrays
-	 for(i = 0; i < count; i++) {
+	 for(i = count - 1; i >= 0; i--) {
 		  printf("%s is %d years old again.\n",
 				 cur_name[i], cur_age[i]);
 	 }
@@ -43,9 +44,9 @@ int main(int argc, char *argv[])
 	 printf("---\n");
 
 	 // fourth way with pointers in a stupid complex way
-	 for(cur_name = names, cur_age = ages;
-		 (cur_age - ages) < count;
-		 cur_name++, cur_age++)
+	 for(cur_name = (names + count - 1), cur_age = (ages + count - 1);
+		 (cur_age - ages) >= 0;
+		 cur_name--, cur_age--)
 	 {
 		  printf("%s lived %d years so far.\n",
 				 *cur_name, *cur_age);
