b2a06d3dcbf9786f08fc48544c1e0117d8bc5e42
Micaiah Chang <ahchang012@gmail.com>
2013-06-17 15:44:01 -0700

    This extra credit is going to be a doozy, better branch

--- ex17.c
+++ ex17.c
@@ -7,22 +7,26 @@
 #include <string.h>
 
 // Define the two below as constants by the C pre-processor. 
-#define MAX_DATA 512
-#define MAX_ROWS 100
+/* #define MAX_DATA 512 */
+/* #define MAX_ROWS 100 */
+
 
 // Declare a structure with the following variables, nothing that the two strings are fixed size and can only have 512 characters in them.
 struct Address {
 	 int id;
 	 int set;
-	 char name[MAX_DATA];
-	 char email[MAX_DATA];
+	 char *name;
+	 char *email;
+
 };
 
 // Make a new structure composed out of MAX_ROWS number of Address structs
 struct Database {
 	 // Imagine a 100 entry python list that you can only add 4-tuples
 	 // to of the form(int, int, "string", "string")
-	 struct Address rows[MAX_ROWS];
+	 const int MAX_DATA;
+	 const int MAX_ROWS;
+	 struct Address *rows;
 };
 
 // A structure that's composed of a pointer to a file
@@ -33,8 +37,10 @@ struct Connection {
 	 struct Database *db;
 };
 
+void Database_close(struct Connection *conn);
+
 // Die function that recieves a string message and returns nothing
-void die(const char *message)
+void die(const char *message, struct Connection *conn)
 {
 	 // If it's a predefined error, that we just saw
 	 // detect it from errno flag...
@@ -49,6 +55,9 @@ void die(const char *message)
 		  // Otherwise, just print your own custom error message
 		  printf("ERROR: %s\n", message);
 	 }
+	 
+	 Database_close(conn); // Free memory
+		  
 
 	 exit(1);
 }
@@ -73,7 +82,7 @@ void Database_load(struct Connection *conn)
 	 // Check to see if it returned 1 piece of data.
 	 // the 1 comes from the THIRD argument in the fread function,
 	 // it is NOT a truth 1. 
-	 if(rc != 1) die("Failed to load database.");
+	 if(rc != 1) die("Failed to load database.", conn);
 }
 
 // Function that returns a pointer of type Connection
@@ -83,14 +92,14 @@ struct Connection *Database_open(const char *filename, char mode)
 	 // First allocate memory for the connection
 	 struct Connection *conn = malloc(sizeof(struct Connection));
 	 // if we fail, cry foul.
-	 if(!conn) die("Memory error"); 
+	 if(!conn) die("Memory error", conn); 
 
 	 // Do the same thing as above for memory.
 	 // Needs to be in this order because the database depends
 	 // on the connection existing and C does not
 	 // automatically allocate dependencies (Important!)
 	 conn->db = malloc(sizeof(struct Database));
-	 if(!conn->db) die("Memory error");
+	 if(!conn->db) die("Memory error", conn);
 
 	 // If our mode argument is 'c' for create...
 	 if(mode == 'c') {
@@ -110,7 +119,7 @@ struct Connection *Database_open(const char *filename, char mode)
 		  // If the pointer is NULL, then wait...
 	 }
 	 // And have it fail here!
-	 if(!conn->file) die("Failed to open the file");
+	 if(!conn->file) die("Failed to open the file", conn);
 	 // Hence we only return when we either can create a file
 	 // or can load a non-NULL file
 	 // Presumptively, this always returns with a pointer
@@ -147,7 +156,7 @@ void Database_write(struct Connection *conn)
 	 // of a Database struct from the connection database in memory
 	 int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
 	 // If you couldn't write 1 element, then you have failed
-	 if(rc != 1) die("Failed to write database");
+	 if(rc != 1) die("Failed to write database", conn);
 
 	 // Flushes stream
 	 // Which means discarding the data inside of the buffer
@@ -155,16 +164,17 @@ void Database_write(struct Connection *conn)
 	 rc = fflush(conn->file);
 	 // If that fails, then die...?
 	 // weird, man fflush says that 0 is success.
-	 if(rc == -1) die("Cannot flush database.");
+	 if(rc == -1) die("Cannot flush database.", conn);
 }
 
 
 // Function that returns nothing and recieves a pointer
 // to a variable of type connection
-void Database_create(struct Connection *conn)
+void Database_create(struct Connection *conn, const int MAX_ROWS, const int MAX_DATA)
 {
 	 int i = 0;
 	 // For every possible row...
+	 conn->db->MAX_ROWS = MAX_ROWS;
 	 for(i = 0; i < MAX_ROWS; i++) {
 		  // make a prototype to initialize it
 		  struct Address addr = {.id = i, .set = 0};
@@ -186,7 +196,7 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
 	 // If the set flag is already 1, then there is already something
 	 // i.e. we cannot overwrite something in the database
 	 // without deleting first. Have to be explicit
-	 if(addr->set) die("Already set, delete it first");
+	 if(addr->set) die("Already set, delete it first", conn);
 
 	 // Say that it is set. Well of course it is, you just set it
 	 addr->set = 1;
@@ -195,13 +205,13 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
 	 addr->name[MAX_DATA-1] = '\0';
 	
 	 // demonstrate the strncpy bug
-	 if(!res) die("Name copy failed");
-
+	 if(!res) die("Name copy failed", conn);
+	 
 	 // Same thing as above
 	 res = strncpy(addr->email, email, MAX_DATA);
 	 
 	 addr->name[MAX_DATA-1] = '\0';
-	 if(!res) die("Email copy failed");
+	 if(!res) die("Email copy failed", conn);
 }
 
 // A function that returns nothing and accepts
@@ -221,7 +231,7 @@ void Database_get(struct Connection *conn, int id)
 	 } else {
 		  // If not it's obviously been not set and send program off
 		  // to die in peace
-		  die("ID is not set");
+		  die("ID is not set", conn);
 	 }
 }
 
@@ -267,7 +277,8 @@ int main(int argc, char *argv[])
 {
 	 // If you don't provide enough arguments, shut down
 	 // And say what format your command needs to be
-	 if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]");
+	 if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]", NULL);
+	 // No connection established so the connection is null
 
 	 // Following the error message
 	 // The first not ex17 arg is the name of the database file
@@ -287,7 +298,7 @@ int main(int argc, char *argv[])
 	 if(argc > 3) id = atoi(argv[3]);
 	 // If that number is bigger than the biggest number of rows
 	 // Then cry and fail
-	 if(id >= MAX_ROWS) die("There's not that many records.");
+	 if(id >= MAX_ROWS) die("There's not that many records.", conn);
 
 	 // Now check the action:
 	 switch(action) {
@@ -302,7 +313,7 @@ int main(int argc, char *argv[])
 		  // When getting something from it
 	 case 'g':
 		  // Only work when you only have 4 entries
-		  if(argc != 4) die("Need an id to get");
+		  if(argc != 4) die("Need an id to get", conn);
 		  // Then try to get the info in the data from the id
 		  Database_get(conn, id);
 		  break;
@@ -310,7 +321,7 @@ int main(int argc, char *argv[])
 		  // When setting aka creating an entry in the database
 	 case 's':
 		  // Check to see you have exactly 6 arguments
-		  if(argc != 6) die("Need id, name, email to set");
+		  if(argc != 6) die("Need id, name, email to set", conn);
 
 		  // Then set stuff using what you think to be
 		  // id, name, email
@@ -322,7 +333,7 @@ int main(int argc, char *argv[])
 		  // Finally, if we want to delete it
 	 case 'd':
 		  // We only need the id so...
-		  if(argc != 4) die("Need id, to delete");
+		  if(argc != 4) die("Need id, to delete", conn);
 
 		  // If we have it, we can delete it with the id
 		  Database_delete(conn, id);
@@ -340,7 +351,7 @@ int main(int argc, char *argv[])
 		  // If your action matches nothing, then simply
 		  // tell the user the right actions
 	 default:
-		  die("Invalid action, only: c=create, g=get, s=set, d=delete");
+		  die("Invalid action, only: c=create, g=get, s=set, d=delete", conn);
 	 }
 	 // Finally, close the database so there are no memory leaks
 	 Database_close(conn);
