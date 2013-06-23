1c1994286cad94a645acaa1dcdff5ca144f0a7c0
Micaiah-Chang <Micaiah@server.fake>
2013-06-23 00:19:55 -0700

    Finished implementing a stack with a linked list. Whoo.

--- stack.c
+++ stack.c
@@ -1,9 +1,16 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
 
-struct Stack {
-	 int *items;
+struct List {
+	 struct Node *head;
+};
+
+struct Node {
+	 char *item;
+	 struct Node *next;
 };
 
 void die(char *message)
@@ -17,34 +24,74 @@ void die(char *message)
 	 exit(1);
 }
 
-void *push(struct Stack *stack, int item)
+struct List *stack_init()
 {
-	 int *new_item = malloc(sizeof(int));
-	 *new_item = item;
-	 int size = sizeof(*(stack->items));
-	 printf("%d \n", size);
-
-	 free(new_item);
-}
-
-int pop(struct Stack *stack)
-{
+	 struct List *lst = malloc(sizeof(struct List));
+	 assert(lst != NULL);
 	 
+	 lst->head = NULL; 
+
+	 return lst;
 }
 
-int top(struct Stack *stack)
+int stack_is_empty(struct List *lst)
 {
-	 
-}
-
-void destroy_stack(struct Stack *stack)
-{
-	 if(stack) {
-		  if(stack->items) {
-			   free(stack->items);
-		  }
-		  free(stack);
+	 if(!lst->head) {
+		  return 1;
+	 } else {
+		  return 0;
 	 }
+}
+
+void stack_push(struct List *lst, char *item)
+{
+	 struct Node *node = malloc(sizeof(struct Node));
+
+	 node->item = strdup(item);
+	 node->next = lst->head;
+
+	 lst->head = node;
+}
+
+struct Node *stack_pop(struct List *lst)
+{
+	 struct Node *node = lst->head;
+	 lst->head = lst->head->next;
+	 
+	 return node;
+}
+
+void stack_peek(struct List *lst)
+{
+	 printf("The top of the stack is: %s\n", lst->head->item);
+}
+
+void destroy_stack(struct List *lst)
+{
+	 if(lst) {
+	 /* WARNING: BELOW NEEDS TO BE CHANGED TO WHILE */
+		  if(lst->head) {
+			   struct Node *curr_node = lst->head;
+			   struct Node *temp = NULL;
+					while(curr_node) {
+						 temp = curr_node->next;
+						 free(curr_node->item);
+						 free(curr_node);
+						 curr_node = temp;
+					}
+		  }
+		  free(lst);
+	 }
+}
+
+void print_stack(struct List *lst)
+{
+	 struct Node *cur_node = lst->head;
+	 while(cur_node) {
+		  printf("%s\t", cur_node->item);
+		  cur_node = cur_node->next;
+	 }
+	 printf("\n");
 }
 
 int main(int argc, char *argv[])
@@ -52,19 +99,31 @@ int main(int argc, char *argv[])
 	 int i = 0;
 	 int count = argc - 1;
 	 char **inputs = argv + 1;
-	 
-	 struct Stack *stck = malloc(sizeof(struct Stack));
-	 
-	 if(argc < 2) die("Please enter an array of numbers");
 
-	 int *numbers = malloc(count * sizeof(int));
-	 if(!numbers) die("Memory error.");
+	 struct List *lst = stack_init();
 
-	 for(i = 0; i < count; i++) {
-		  numbers[i] = atoi(inputs[i]);
-		  push(stck, numbers[i]);
+	 if(stack_is_empty(lst)) {
+		  printf("Stack is empty.\n");
+	 } else {
+		  printf("Stack is not empty.\n");
+	 }
+	 
+	 for(i = 0; i < count; i++)	 stack_push(lst, inputs[i]);
+	 print_stack(lst);
+
+	 stack_peek(lst);
+	 
+	 if(stack_is_empty(lst)) {
+		  printf("Stack is empty.\n");
+	 } else {
+		  printf("Stack is not empty.\n");
 	 }
 
-	 free(numbers);
-	 destroy_stack(stck);
+	 struct Node *node = stack_pop(lst);
+	 printf("Popped item: %s\n", node->item);
+	 free(node->item);
+	 free(node);
+	 print_stack(lst);
+	 
+	 destroy_stack(lst);
 }
