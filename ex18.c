2010a213485e10efe33093f6ddab463e8f7d1c70
Micaiah-Chang <Micaiah@server.fake>
2013-06-24 23:27:33 -0700

    Whew, exercise 18 completely done

--- ex18.c
+++ ex18.c
@@ -19,6 +19,38 @@ void die(const char *message)
 // a typedef creates a fake type, in this
 // case for a function pointer
 typedef int (*compare_cb)(int a, int b);
+typedef int *(*sort)(int *numbers, int count, compare_cb cmp);
+
+int *insert_sort(int *numbers, int count, compare_cb cmp)
+{
+
+     int i = 0;
+     int j = 0;
+     int temp = 0;
+     int *target = malloc(count * sizeof(int));
+
+	 if(count <= 1) return *numbers;
+     if(!target) die("Memory error.");
+
+     memcpy(target, numbers, count * sizeof(int));
+
+     int hole = 0;
+     
+     for(i = 0; i < count; i++)
+     {
+	  temp = target[i];
+	  hole = i;
+	  while(hole > 0 && (cmp(target[hole-1], temp) > 0)) {
+	       target[hole] = target[hole-1];
+	       hole--;
+	  }
+	  target[hole] = temp;
+     }
+
+	 
+	 return target;
+     
+}
 
 /**
  *A classic bubble sort function that uses the
@@ -29,11 +61,14 @@ int *bubble_sort(int *numbers, int count, compare_cb cmp)
 	 int temp = 0;
 	 int i = 0;
 	 int j = 0;
+	 
+	 if(count <= 1) return *numbers;
+
 	 int *target = malloc(count * sizeof(int));
 
 	 if(!target) die("Memory error.");
 
-	 memcpy(target, numbers, count* sizeof(int));
+	 memcpy(target, numbers, count * sizeof(int));
 	 
 	 for(i = 0; i < count; i++) {
 		  for(j = 0; j < count - 1; j++) {
@@ -71,10 +106,10 @@ int strange_order(int a, int b)
  * Used to test that we are sorting things correctly
  * by doing the sort and printing it out
  */
-void test_sorting(int *numbers, int count, compare_cb cmp)
+void test_sorting(int *numbers, int count, compare_cb cmp, sort alg)
 {
 	 int i = 0;
-	 int *sorted = bubble_sort(numbers, count, cmp);
+	 int *sorted = alg(numbers, count, cmp);
 
 	 if(!sorted) die("Failed to sort as requested");
 
@@ -85,13 +120,13 @@ void test_sorting(int *numbers, int count, compare_cb cmp)
 	 
 	 free(sorted);
 
-	 unsigned char *data = (unsigned char *)cmp;
+	 /* unsigned char *data = (unsigned char *)cmp; */
 
-	 for(i = 0; i < 25; i++) {
-		  printf("%02x:", data[i]);
-	 }
+	 /* for(i = 0; i < 25; i++) { */
+	 /* 	  printf("%02x:", data[i]); */
+	 /* } */
 
-	 printf("\n");
+	 /* printf("\n"); */
 }
 
 
@@ -110,10 +145,24 @@ int main(int argc, char *argv[])
 		  numbers[i] = atoi(inputs[i]);
 	 }
 
-	 test_sorting(numbers, count, sorted_order);
-	 test_sorting(numbers, count, reverse_order);
-	 test_sorting(numbers, count, strange_order);
+	 printf("bubble_sort: ");
+	 test_sorting(numbers, count, sorted_order, bubble_sort);
+	 printf("insert_sort: ");
+	 test_sorting(numbers, count, sorted_order, insert_sort);
+
+
+	 printf("bubble_sort: ");
+	 test_sorting(numbers, count, reverse_order, bubble_sort);
+	 printf("insert_sort: ");
+	 test_sorting(numbers, count, reverse_order, insert_sort);
+
+
+	 printf("bubble_sort: ");
+	 test_sorting(numbers, count, strange_order, bubble_sort);
+	 printf("insert_sort: ");
+	 test_sorting(numbers, count, strange_order, insert_sort);
 	 // test_sorting(numbers, count , die);
+	 // test_sorting(numbers, count, NULL);
 
 	 free(numbers);
 
--- test18.sh
+++ test18.sh
@@ -2,4 +2,14 @@
 
 set -e
 make ex18
-./bin/ex18 4 3 2 1 6 8
+echo "Valgrind? 'y'"
+read valgrind
+if [ $valgrind = "y" ]; then
+    prefix="valgrind "
+    echo "Leak test? Y/N"
+    read leak
+    if [ $leak = "y" ]; then
+	prefix="valgrind --leak-check=yes "
+    fi
+fi
+$prefix ./bin/ex18 4 3 2 1 6 8
