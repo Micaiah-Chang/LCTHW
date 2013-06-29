187557f017e1bbaef713d1fe469adfbaf3d0f327
Micaiah-Chang <Micaiah@server.fake>
2013-06-28 20:29:42 -0700

    Created a separate file for the mechanics of the game in preparation for a new map

--- game/ex19.c
+++ game/ex19.c
@@ -3,99 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
 #include "ex19.h"
 #include <assert.h>
-
-int Monster_attack(void *self, int damage)
-{
-	 assert(self != NULL);
-	 assert(damage >= 0);
-	 
-	 Monster *monster = self;
-
-	 printf("You attack %s!\n", monster->_(description));
-
-	 monster->hit_points -= damage;
-
-	 if(monster->hit_points > 0) {
-		  printf("It is still alive.\n");
-		  return 0;
-	 } else {
-		  printf("It is dead!\n");
-		  return 1;
-	 }
-}
-
-int Monster_init(void *self)
-{
-	 assert(self != NULL);
-	 Monster *monster = self;
-	 monster->hit_points = 10;
-	 return 1;
-}
-
-Object MonsterProto = {
-	 .init = Monster_init,
-	 .attack = Monster_attack
-};
-
-
-void *Room_move(void *self, Direction direction)
-{
-	 assert(self != NULL);
-	 assert((direction >= 0) && (direction < 4));
-
-	 Room *room = self;
-	 Room *next = NULL;
-
-	 if(direction == NORTH && room->north) {
-		  printf("You go north, into:\n");
-		  next = room->north;
-	 } else if(direction == SOUTH && room->south) {
-		  printf("You go south, into:\n");
-		  next = room->south;
-	 } else if(direction == EAST && room->east) {
-		  printf("You go east, into:\n");
-		  next = room->east;
-	 } else if(direction == WEST && room->west) {
-		  printf("You go west, into:\n");
-		  next = room->west;
-	 } else {
-		  printf("You can't go that direction.");
-		  next = NULL;
-	 }
-
-	 if(next) {
-		  next->_(describe)(next);
-	 }
-
-	 return next;
-}
-
-
-int Room_attack(void *self, int damage)
-{
-	 assert(self != NULL);
-	 assert(damage >= 0);
-	 
-	 Room *room = self;
-	 Monster *monster = room->bad_guy;
-
-	 if(monster) {
-		  monster->_(attack)(monster, damage);
-		  return 1;
-	 } else {
-		  printf("You flail in the air at nothing. Idiot.\n");
-		  return 0;
-	 }
-}
-
-
-Object RoomProto = {
-	 .move = Room_move,
-	 .attack = Room_attack
-};
-
 
 void *Map_move(void *self, Direction direction)
 {
@@ -146,10 +56,15 @@ int Map_init(void *self)
 
 	 Room *kitchen = NEW(Room, "Kitchen, you have the knife now");
 	 assert(kitchen);
+
+	 Room *pantry = NEW(Room, "A pantry, it smells of jerky and of the goblin there");
 	 
 	 // put the bad guy in the arena
 	 arena->bad_guy = NEW(Monster, "The evil minotaur");
 	 assert(arena->bad_guy != NULL);
+
+	 pantry->bad_guy = NEW(Monster, "A scraggly goblin");
+	 assert(pantry->bad_guy != NULL);
 
      // setup the map rooms
 	 hall->north = throne;
@@ -160,6 +75,10 @@ int Map_init(void *self)
 
 	 arena->east = throne;
 	 kitchen->west = throne;
+
+	 kitchen->north = pantry;
+
+	 pantry->south = kitchen;
 
 	 // start the map and the character off in the hall
 	 map->start = hall;
@@ -174,54 +93,6 @@ Object MapProto = {
 	 .attack = Map_attack
 };
 
-int process_input(Map *game)
-{
-	 printf("\n> ");
-
-	 char ch = getchar();
-	 getchar(); // eat ENTER
-
-	 int damage = rand() % 4;
-
-	 switch(ch) {
-	 case -1:
-		  printf("Giving up? You suck.\n");
-		  return 0;
-		  break;
-
-	 case 'n':
-		  game->_(move)(game, NORTH);
-		  break;
-
-	 case 's':
-		  game->_(move)(game, SOUTH);
-		  break;
-
-	 case 'e':
-		  game->_(move)(game, EAST);
-		  break;
-
-	 case 'w':
-		  game->_(move)(game, WEST);
-		  break;
-
-	 case 'a':
-		  game->_(attack)(game, damage);
-		  break;
-	 case 'l':
-		  printf("You can go:\n");
-		  if(game->location->north) printf("NORTH\n");
-		  if(game->location->south) printf("SOUTH\n");
-		  if(game->location->east) printf("EAST\n");
-		  if(game->location->west) printf("WEST\n");
-		  break;
-
-	 default:
-		  printf("What?: %d\n", ch);
-	 }
-
-	 return 1;
-}
 
 int main(int argc, char *argv[])
 {
--- game/test.sh
+++ game/test.sh
@@ -2,8 +2,5 @@
 
 
 make ex19
-echo l a n e \
-	w e l w \
-	e w w a \
-	a a a a \
-	a a a a | ./ex19 
+echo l a n e w e l w e w w a a a a a a a a e e n a a a a a a a a | ./ex19 
+make clean
