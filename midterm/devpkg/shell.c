e6a73e36c9ebd3e585aedb0351d1ebd4a3c2c535
Micaiah-Chang <Micaiah@server.fake>
2013-07-19 01:49:07 -0700

    Finished code reviewing for the shell functions and added functionality for checking number of arguments that need to be replaced.

--- midterm/devpkg/shell.c
+++ midterm/devpkg/shell.c
@@ -11,6 +11,7 @@ int Shell_exec(Shell template, ...)
 	 const char *key = NULL;
 	 const char *arg = NULL;
 	 int i = 0;
+	 int args_replaced = 0;
 
 	 rv = apr_pool_create(&p, NULL);
 	 check(rv == APR_SUCCES, "Failed to create pool.");
@@ -26,11 +27,13 @@ int Shell_exec(Shell template, ...)
 		  for(i = 0; template. args[i] != NULL; i++) {
 			   if(strcmp(template.args[i], key) == 0) {
 					template.args[i] = arg;
+					args_replaced++;
 					break;
 			   }
 		  }
 	 }
 
+	 check(args_replaced == template.var_args, "Did not replace the right number of arguments!");
 	 rc_Shell_run(p, *template);
 	 apr_pool_destroy(p);
 	 va_end(argp);
@@ -47,13 +50,12 @@ int Shell_run(apr_pool_t *p, Shell *cmd)
 {
 	 apr_proattr_t *attr;
 	 apr_status_t rv;
-	 apr_proc_t rv;
 	 apr_proc_t newproc;
 
 	 rv = apr_procattr_create(&attr, p);
 	 check(rv == APR_SUCCESS, "Failed to create proc attr.");
 
-	 rv = apr_procattr_io_set(attr, APR_NO_PIPE, APR_NO_PEIPE,
+	 rv = apr_procattr_io_set(attr, APR_NO_PIPE, APR_NO_PIPE,
 		  APR_NO_PIPE);
 	 check(rv == APR_SUCESS, "Failed to set IO of command");
 
@@ -82,31 +84,36 @@ Shell CLEANUP_SH = {
 	 .exe = "rm",
 	 .dir = "/tmp",
 	 .args = {"rm", "-rf", "/tmp/pkg-build", "/tmp/pkg-src.tar.gz",
-			  "/tmp/pkg-src.tar.bz2", "/tmp/DEPENDS", NULL}
+			  "/tmp/pkg-src.tar.bz2", "/tmp/DEPENDS", NULL},
+	 .var_args = 1
 };
 
 Shell GIT_SH = {
 	 .dir = "/tmp",
 	 .exe = "git",
-	 .args = {"git", "clone", "URL", "pkg-build", NULL}
+	 .args = {"git", "clone", "URL", "pkg-build", NULL},
+	 .var_args = 1
 };
 
 Shell TAR_SH = {
 	 .dir = "/tmp/pkg-build",
 	 .exe = "tar",
-	 .args = {"tar", "-xzf", "FILE", "--strip-components", "1", NULL}
+	 .args = {"tar", "-xzf", "FILE", "--strip-components", "1", NULL},
+	 .var_args = 1
 };
 
 Shell CURL_SH = {
 	 .dir = "/tmp",
 	 .exe = "curl",
-	 .args = {"curl", "-L", "-o", "TARGET", "URL", NULL}
+	 .args = {"curl", "-L", "-o", "TARGET", "URL", NULL},
+	 .var_args = 2
 };
 
 Shell CONFIGURE_SH = {
-	 .exe = ".configure",
+	 .exe = "./configure",
 	 .dir = "/tmp/pkg-build",
 	 .args = {"configure", "OPTS", NULL},
+	 .var_args = 1
 };
 
 
@@ -114,10 +121,12 @@ Shell MAKE_SH = {
 	 .exe = "make",
 	 .dir = "tmp/pkg-build",
 	 .args = {"make", "OPTS", NULL}
+	 .var_args = 1
 };
 
 Shell INSTALL_SH = {
 	 .exe = "sudo",
 	 .dir = "/tmp/pkg-build",
 	 .args = {"sudo", "make", "TARGET", NULL}
+	 .var_args = 1
 };
--- midterm/devpkg/shell.h
+++ midterm/devpkg/shell.h
@@ -15,6 +15,7 @@ typedef struct Shell {
 	 int exit_code;
 
 	 const char *args[MAX_COMMAND_ARGS];
+	 const int var_args;
 } Shell;
 
 int Shell_run(apr_pool_t *p, Shell *cmd);
