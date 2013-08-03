8fa2197f6f129bd25fd85b4e22453a0137833f6a
Micaiah-Chang <Micaiah@server.fake>
2013-08-02 20:34:53 -0700

    Finished ex30, going to clean up directory for general use now.

--- ex29-project/tests/runtests.sh
+++ ex29-project/tests/runtests.sh
@@ -4,12 +4,15 @@ for i in tests/*_tests
 do
 	if test -f $i
 	then
-		TESTLIB="$i ./build/libex29.so"
-		$TESTLIB print_a_message "Hi" 2
-		$TESTLIB uppercase "hello world" 11
-		$TESTLIB lowercase "HELLO there" 11
-		$TESTLIB fail_on_purpose 5 
-		$TESTLIB asdfsg asdfasgs 5
+		if $VALGRIND ./$i 2>> tests/tests.log
+		then
+			echo $i PASS
+		else
+			echo "ERROR in test $i: here's tests/test.log"
+			echo "-------"
+			tail tests/tests.log
+			exit 1
+		fi
 	fi
 done
 
