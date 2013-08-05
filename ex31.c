f88e547acb31db83b739b8f8d5404e3d656ad4bf
Micaiah-Chang <Micaiah@server.fake>
2013-08-04 19:17:15 -0700

    Purposefully dereferencing a null pointer in order to create a core dump for gdb to analyze. Done with ex31 in full

--- ex31.c
+++ ex31.c
@@ -5,9 +5,14 @@
 int main(int argc, char *argv[])
 {
 	 int i = 0;
+	 int *thing = NULL;
 	 
 	 while (i < 100) {
 		  usleep(3000);
+		  if(i > 10) {
+			   thing[i] = 1;
+		  }
+		  i++;
 	 }
 	 
 	 return 0;
