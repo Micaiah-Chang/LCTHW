3b97f7bf9fda0e699f6220df12bac70d5fd66037
Micaiah-Chang <Micaiah@server.fake>
2013-08-18 16:14:04 -0700

    Implement inplace recursive quicksort, with its own unit test format.
    Made quicksort with Wikipedia's implementation of the in place quicksort.
    In order to get the unit test to pass, used the strcmp as the comparison
    directly instead of utilizing the function passed in. Will need to figure
    out how to do the comparison with the test_cmp function in the unit test.

--- liblcthw/src/lcthw/darray_algos.c
+++ liblcthw/src/lcthw/darray_algos.c
@@ -11,24 +11,56 @@ int DArray_qsort(DArray *array, DArray_compare cmp)
 
 int DArray_my_qsort(DArray *array, DArray_compare cmp)
 {
-	  /*  function quicksort(array, left, right) */
- 
-      /* // If the list has 2 or more items */
-      /* if left < right */
- 
-      /*     // See "Choice of pivot" section below for possible choices */
-      /*     choose any pivotIndex such that left ≤ pivotIndex ≤ right */
- 
-      /*     // Get lists of bigger and smaller items and final position of pivot */
-      /*     pivotNewIndex := partition(array, left, right, pivotIndex) */
- 
-      /*     // Recursively sort elements smaller than the pivot */
-      /*     quicksort(array, left, pivotNewIndex - 1) */
- 
-      /*     // Recursively sort elements at least as big as the pivot */
-      /*     quicksort(array, pivotNewIndex + 1, right) */
-	 qsort(array->contents, DArray_count(array), sizeof(void *), cmp);
+	 my_qsort(array, 0, array->end - 1, cmp);
 	 return 0;
+}
+
+int partition(DArray *array, int left, int right, int pivot, DArray_compare cmp)
+{
+	 assert(pivot >= left && pivot =< right &&
+			"pivot outside of acceptable range.");
+	 assert(array != NULL && "array cannot be NULL");
+	 
+	 void *pivot_value = array->contents[pivot];
+	 array->contents[pivot] = array->contents[right];
+	 array->contents[right] = pivot_value;
+	 int index = left;
+	 void *temp = NULL;
+	 int i = 0;
+
+	 
+	 for(i = 0; i < right; i++) {
+		  if(strcmp(array->contents[i], pivot_value) < 0) {
+			   temp = array->contents[i];
+			   array->contents[i] = array->contents[index];
+			   array->contents[index] = temp;
+			   index++;
+		  }
+	 }
+	 temp = array->contents[index];
+	 array->contents[index] = array->contents[right];
+	 array->contents[right] = temp;
+
+	 return index;
+}
+
+DArray *my_qsort(DArray *array, int left, int right, DArray_compare cmp)
+{
+	 assert(array != NULL && "Array cannot be NULL");
+	 assert(left <= right && "Left value cannot be greater than right!");
+	 assert(left >= 0 && right >= && "Left and right values cannot be negative!");
+	 
+	 if(left < right) {
+		  int pivot = right;
+
+		  pivot = partition(array, left, right, pivot, cmp);
+
+		  my_qsort(array, left, pivot - 1, cmp);
+		  my_qsort(array, pivot + 1, right, cmp);
+	 }
+
+	 
+	 return array;
 }
 
 int DArray_heapsort(DArray *array, DArray_compare cmp)
--- liblcthw/src/lcthw/darray_algos.h
+++ liblcthw/src/lcthw/darray_algos.h
@@ -5,6 +5,9 @@
 
 typedef int (*DArray_compare)(const void *a, const void *b);
 
+int partition(DArray *array, int left, int right, int pivot, DArray_compare cmp);
+DArray *my_qsort(DArray *array, int left, int right, DArray_compare cmp);
+
 int DArray_qsort(DArray *array, DArray_compare cmp);
 int DArray_my_qsort(DArray *array, DArray_compare cmp);
 
